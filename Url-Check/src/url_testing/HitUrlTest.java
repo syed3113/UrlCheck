43a54e0e65792d1d67583512a06839f063e571e5
syed3113 <47118423+syed3113@users.noreply.github.com>
2019-12-06 19:02:32 +0530

    TC

--- Url-Check/src/url_testing/HitUrlTest.java
+++ Url-Check/src/url_testing/HitUrlTest.java
@@ -71,8 +71,8 @@ public class HitUrlTest {
 			Duration timeElapsed = Duration.between(start, end);
 			System.out.println("Time taken: " + (timeElapsed.toMillis()) / 1000 + " seconds");
 			Xlfile_Reader.setCellData1((timeElapsed.toMillis()) / 1000, i, 2);
-			driver.close();
+			
 		}
-
+                        driver.close();
 	}
 }
