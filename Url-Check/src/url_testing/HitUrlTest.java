b31e3a53958dbf650d33c7080aa2747ab207a45a
syed3113 <47118423+syed3113@users.noreply.github.com>
2019-12-10 21:37:45 +0530

    Loop changed

--- Url-Check/src/url_testing/HitUrlTest.java
+++ Url-Check/src/url_testing/HitUrlTest.java
@@ -18,7 +18,7 @@ public class HitUrlTest {
 	@Test
 	public void hitUrlTest() throws Exception {
 		Xlfile_Reader.setExcelFile("../Url-Check/src/url_testing/Lotame_laptop_pages.xls", "Sheet1");
-		for (int i = 102; i <=178; i++) {
+		for (int i = 155; i <=178; i++) {
 			//System.out.println(Xlfile_Reader.getRowCount("Sheet1"));
 			String url = Xlfile_Reader.getCellData(i, 0);
 			System.out.println("Url:-" + url);
